857fb2e07e1a928a2148940e62e6788ce15b419e
tijan <tijanmdr@gmail.com>
2022-12-29 18:09:06 +1100

    fixes on jwt

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('r', 'UserController@index');
+
+Route::group(['middleware'=>['jwt.verify']], function () {
+    Route::get('t', function () {
+        return 0;
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'UserController@index']);
+Route::get('/', 'UserController@index');
