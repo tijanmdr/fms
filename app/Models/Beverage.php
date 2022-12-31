ce5a6df8e13d52a3e8b22a8297effdb8f911fb91
tijan <tijanmdr@gmail.com>
2022-12-31 22:53:50 +1100

    models for all the schema done

--- app/Models/Beverage.php
+++ app/Models/Beverage.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Beverage extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'photo',
+        'allergic',
+        'allergic_id',
+        'ingredients',
+        'sauce',
+        'hide',
+    ];
 }
--- app/Models/Food.php
+++ app/Models/Food.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Food extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'photo',
+        'allergic',
+        'allergic_id',
+        'ingredients',
+        'price',
+        'hot',
+        'sauce',
+        'hide',
+    ];
 }
