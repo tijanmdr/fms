b7b0acd486ac66b027ea947cb1fa0bdc1f37a44c
tijan <tijanmdr@gmail.com>
2023-01-08 15:22:25 +1100

    routes and controllers created

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,36 @@ Route::get('login', 'UserController@check');
 Route::post('login', 'UserController@login');
 
 Route::group(['middleware'=>['jwt.verify']], function () {
+    // User routes
     Route::get('user_detail', 'UserController@userDetail');
     Route::post('add_user', 'UserController@addUser');
     Route::patch('update_user', 'UserController@updateUser');
     Route::patch('update_user_pwd', 'UserController@updateUserPassword');
     Route::get('list_users', 'UserController@listUsers');
+
+    // Food categories routes
+    Route::post('create_category', 'FoodCategorySauceController@createCategory');
+    Route::patch('update_category', 'FoodCategorySauceController@updateCategory');
+    Route::delete('delete_category', 'FoodCategorySauceController@deleteCategory');
+    Route::list('list_categories', 'FoodCategorySauceController@listCategories');
+
+    // Food sauces routes
+    Route::post('create_sauce', 'FoodCategorySauceController@createSauce');
+    Route::patch('update_sauce', 'FoodCategorySauceController@updateSauce');
+    Route::delete('delete_sauce', 'FoodCategorySauceController@deleteSauce');
+    Route::list('list_sauces', 'FoodCategorySauceController@listSauces');
+
+    // Food menu routes
+    Route::post('create_food', 'FoodBeverageController@createFood');
+    Route::patch('update_food', 'FoodBeverageController@updateFood');
+    Route::delete('delete_food', 'FoodBeverageController@deleteFood');
+    Route::list('list_foods', 'FoodBeverageController@listFoods');
+
+    // Food beverages routes
+    Route::post('create_beverage', 'FoodBeverageController@createBeverage');
+    Route::patch('update_beverage', 'FoodBeverageController@updateBeverage');
+    Route::delete('delete_beverage', 'FoodBeverageController@deleteBeverage');
+    Route::list('list_beverages', 'FoodBeverageController@listBeverages');
 });
 
 // 404 error in json
