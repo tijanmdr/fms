b4a1880fe31a4cfeb14374bf55758bd15511fae0
tijan <tijanmdr@gmail.com>
2023-01-09 18:16:30 +1100

    made some entities fillable for insertion of data

--- app/Http/Controllers/FoodBeverageController.php
+++ app/Http/Controllers/FoodBeverageController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Food;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FoodBeverageController extends Controller
 {
-    //
+    public function createFood(Request $req) {
+        $req = $req->only('name', 'before', 'after');
+        $validate = Validator::make($req, [
+            'name' => 'required|string|unique:foods,name',
+            'photo' => 'required',
+            'ingredients' => 'required',
+            'price' => 'required|double',
+            'hot' => 'integer',
+            'sauce' => 'string',
+            'category' => 'required|exists:categories,id',
+        ]);
+        
+        if ($validate->fails()) {
+            $error = validateErrorMessage($validate);
+            return returnMessage(false, $error);
+        }    
+
+    }
+
+    public function updateFood(Request $req) {
+
+    }
+
+    public function deleteFood() {
+
+    }
+
+    public function listFoods() {
+
+    }
+
+    public function createBeverage(Request $req) {
+
+    }
+
+    public function updateBeverage(Request $req) {
+
+    }
+
+    public function deleteBeverage() {
+
+    }
+
+    public function listBeverages() {
+
+    }
 }
